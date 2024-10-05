5431d60fe8199c0c5c9250e8e1694d7fc3cc5df3
qoldaqgit <152833825+qoldaqgit@users.noreply.github.com>
2024-10-04 21:22:56 -0400

    Update create-ubuntu-minimal-vm.sh

--- Promox/create-ubuntu-minimal-vm.sh
+++ Promox/create-ubuntu-minimal-vm.sh
@@ -1,6 +1,6 @@
 if [[ $# -eq 1 ]]
 then
-wget https://cloud-images.ubuntu.com/releases/22.04/release/ubuntu-22.04-server-cloud>
+wget https://cloud-images.ubuntu.com/releases/22.04/release/ubuntu-22.04-server-cloudimg-amd64.img
 qm create $1 --memory 2048 --name ubuntu-cloud --net0 virtio,bridge=vmbr0
 qm disk import $1 ubuntu-22.04-server-cloudimg-amd64.img local-lvm
 qm set $1 --scsihw virtio-scsi-pci --scsi0 local-lvm:vm-$1-disk-0
