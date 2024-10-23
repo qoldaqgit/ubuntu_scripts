6e20e7e6cd4659da8f9c37ede079ec34f0fd55eb
qoldaqgit <152833825+qoldaqgit@users.noreply.github.com>
2024-10-23 07:59:41 -0400

    Update MariaDB.sh

--- Apps/MariaDB.sh
+++ Apps/MariaDB.sh
@@ -1,28 +1,29 @@
 sudo apt update && sudo apt upgrade -y
-sudo apt install qemu-guest-agent
-
-
-sudo apt install mariadb-server mariadb-client -y
+#Install MariaDB
+sudo apt install mariadb-server -y
 sudo mysql_secure_installation
 
-#Change the root password? [Y/n] n
+#Switch to unix_socket authentication [Y/n] n
+#Change the root password? [Y/n] n 
 #Remove anonymous users? [Y/n] y
 #Disallow root login remotely? [Y/n] y
 #Remove test database and access to it? [Y/n] y
 #Reload privilege tables now? [Y/n] y
 
+
+#create a new user by:
 sudo mysql
-  CREATE USER 'newuser'@'localhost' IDENTIFIED BY '1234';
-  GRANT ALL PRIVILEGES ON *.* TO 'newuser'@'localhost';
+  CREATE USER 'newuser'@'localhost' IDENTIFIED BY 'password';
+  GRANT ALL PRIVILEGES ON *.* TO 'newuser'@'localhost' WITH GRANT OPTION;
   FLUSH PRIVILEGES;
 
 
-CREATE USER 'dbmaster'@'%' IDENTIFIED VIA mysql_native_password USING '1234';
-GRANT ALL PRIVILEGES ON *.* TO 'dbmaster'@'%';
+#CREATE USER 'dbmaster'@'%' IDENTIFIED VIA mysql_native_password USING '1234';
+#GRANT ALL PRIVILEGES ON *.* TO 'dbmaster'@'%';
 
-sudo sed -i 's/bind-address = 127.0.0.1/bind-address = 0.0.0.0/g' /etc/mysql/mariadb.conf.d/50-server.cnf
+#sudo sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mysql/mariadb.conf.d/50-server.cnf
 
 
 sudo apt install apache2 php libapache2-mod-php php-mysql phpmyadmin php-mbstring php-zip php-gd php-json php-curl -y
 
-sudo ufw allow 3306/tcp
+
