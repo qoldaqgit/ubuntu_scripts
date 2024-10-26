7fa8fe4e9badf1b369f3b0c99a2f6bf66658ca10
qoldaqgit <152833825+qoldaqgit@users.noreply.github.com>
2024-10-26 10:36:49 -0400

    Update MariaDB.sh

--- Apps/MariaDB.sh
+++ Apps/MariaDB.sh
@@ -21,9 +21,9 @@ sudo mysql
 #CREATE USER 'dbmaster'@'%' IDENTIFIED VIA mysql_native_password USING '1234';
 #GRANT ALL PRIVILEGES ON *.* TO 'dbmaster'@'%';
 
-#sudo sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mysql/mariadb.conf.d/50-server.cnf
 
 
 sudo apt install apache2 php libapache2-mod-php php-mysql phpmyadmin php-mbstring php-zip php-gd php-json php-curl -y
 
+sudo sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mysql/mariadb.conf.d/50-server.cnf
 
