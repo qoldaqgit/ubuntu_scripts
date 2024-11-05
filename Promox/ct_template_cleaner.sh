6b79f3e2bb441eb266e7b13e9a15ad3937110f4c
qoldaqgit <152833825+qoldaqgit@users.noreply.github.com>
2024-11-05 15:20:40 -0500

    Create ct_template_cleaner.sh

---
+++ Promox/ct_template_cleaner.sh
@@ -0,0 +1,15 @@
+#/bin/bash
+if [[ $# -eq 1 ]]
+then
+sudo apt update && sudo apt upgrade -y
+sudo adduser $1
+sudo usermod aG sudo $1
+sudo apt clean
+sudo apt autoremove
+sudo rm /etc/ssh/ssh_host_*
+sudo trunncate -ss 0 /etc/machine-id
+sudo shutdown 0
+
+else
+echo "please provide a [[username]]"
+fi
