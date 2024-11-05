eb1c41cfee984f4bf0d4c1bbfc57020db5677f00
qoldaqgit <152833825+qoldaqgit@users.noreply.github.com>
2024-11-05 17:12:53 -0500

    Update ct_template_cleaner.sh

--- Promox/ct_template_cleaner.sh
+++ Promox/ct_template_cleaner.sh
@@ -4,8 +4,8 @@
 if [[ $# -eq 1 ]]
 then
 sudo apt update && sudo apt upgrade -y
-sudo adduser $1
-sudo usermod -aG sudo $1
+#sudo adduser $1
+#sudo usermod -aG sudo $1
 sudo apt clean
 sudo apt autoremove
 sudo rm /etc/ssh/ssh_host_*
