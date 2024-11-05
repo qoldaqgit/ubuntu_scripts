079072b234dd016e915f3ec3f940f11c47881418
qoldaqgit <152833825+qoldaqgit@users.noreply.github.com>
2024-11-05 16:35:19 -0500

    Update ct_template_cleaner.sh

--- Promox/ct_template_cleaner.sh
+++ Promox/ct_template_cleaner.sh
@@ -1,15 +1,20 @@
 #/bin/bash
 
-# bash -c "$(wget -qLO - https://raw.githubusercontent.com/qoldaqgit/ubuntu_scripts/refs/heads/main/Promox/ct_template_cleaner.sh)"
+# wget https://raw.githubusercontent.com/qoldaqgit/ubuntu_scripts/refs/heads/main/Promox/ct_template_cleaner.sh
 if [[ $# -eq 1 ]]
 then
 sudo apt update && sudo apt upgrade -y
 sudo adduser $1
-sudo usermod aG sudo $1
+sudo usermod -aG sudo $1
 sudo apt clean
 sudo apt autoremove
 sudo rm /etc/ssh/ssh_host_*
-sudo trunncate -ss 0 /etc/machine-id
+sudo truncate -s 0 /etc/machine-id
+
+touch ssh_config.sh
+echo "sudo dpkg-reconfigure openssh-server" > ssh_config.sh
+
+rm ct_template_cleaner.sh
 sudo shutdown 0
 
 else
