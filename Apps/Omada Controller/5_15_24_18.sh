4824b9707c3191becc4c276e2f58efad8ac30897
qoldaqgit <152833825+qoldaqgit@users.noreply.github.com>
2025-07-19 01:05:59 -0400

    Update 5_15_24_18.sh

--- Apps/Omada Controller/5_15_24_18.sh
+++ Apps/Omada Controller/5_15_24_18.sh
@@ -11,14 +11,3 @@ echo '##                                                   ##'
 echo '#######################################################'
 
 apt update && apt upgrade -y
-apt install openjdk-8-jre-headless gnupg2 jsvc curl -y
-
-curl -fsSL https://www.mongodb.org/static/pgp/server-7.0.asc | \
-   sudo gpg -o /usr/share/keyrings/mongodb-server-7.0.gpg \
-   --dearmor
-
-echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-7.0.gpg ] https://repo.mongodb.org/apt/ubuntu jammy/mongodb-org/7.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-7.0.list
-sudo apt update
-sudo apt install -y mongodb-org=7.0.14 mongodb-org-database=7.0.14 mongodb-org-server=7.0.14 mongodb-mongosh mongodb-org-mongos=7.0.14 mongodb-org-tools=7.0.14
-wget https://static.tp-link.com/upload/software/2025/202507/20250703/omada_v5.15.24.18_linux_x64_20250630184434.deb
-sudo dpkg -i omada_v5.15.24.18_linux_x64_20250630184434.deb
