47d62dea294520316ef179e0e3c5145726081742
qoldaqgit <152833825+qoldaqgit@users.noreply.github.com>
2025-08-12 17:59:07 -0400

    Update OpenProject.sh

--- containers/v2/OpenProject.sh
+++ containers/v2/OpenProject.sh
@@ -2,13 +2,13 @@ mkdir -p openproject/{data,assets}
 cd openproject
 touch docker-compose.yml
 
-echo 'version: 3.8
+echo "version: '3.8'
 services:
   openproject:
     image: openproject/openproject:16
     container_name: openproject
     ports:
-      - "8180:80"
+      - '8180:80'
     environment:
       OPENPROJECT_HOST__NAME: 10.10.10.30:8180
       OPENPROJECT_SECRET_KEY_BASE: secret
@@ -16,4 +16,4 @@ services:
       - ~/openproject/data:/var/openproject/pgdata
       - ~/openproject/assets:/var/openproject/assets
     restart: unless-stopped
-' > ~/openproject/docker-compose.yml
+" > ~/openproject/docker-compose.yml
