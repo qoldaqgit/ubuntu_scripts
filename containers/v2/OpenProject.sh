188fead88e64238730c0ec5be067e05e6755dbb9
qoldaqgit <152833825+qoldaqgit@users.noreply.github.com>
2025-08-12 17:58:06 -0400

    Create OpenProject.sh

---
+++ containers/v2/OpenProject.sh
@@ -0,0 +1,19 @@
+mkdir -p openproject/{data,assets}
+cd openproject
+touch docker-compose.yml
+
+echo 'version: 3.8
+services:
+  openproject:
+    image: openproject/openproject:16
+    container_name: openproject
+    ports:
+      - "8180:80"
+    environment:
+      OPENPROJECT_HOST__NAME: 10.10.10.30:8180
+      OPENPROJECT_SECRET_KEY_BASE: secret
+    volumes:
+      - ~/openproject/data:/var/openproject/pgdata
+      - ~/openproject/assets:/var/openproject/assets
+    restart: unless-stopped
+' > ~/openproject/docker-compose.yml
