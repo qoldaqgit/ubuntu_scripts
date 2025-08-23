ce1f9c83420247f573907395a4034a269a4e7df2
qoldaqgit <152833825+qoldaqgit@users.noreply.github.com>
2025-08-23 14:39:33 -0400

    Update podman_install.sh

--- Apps/Podman/podman_install.sh
+++ Apps/Podman/podman_install.sh
@@ -11,49 +11,12 @@ sudo apt-get -y install podman podman-compose
 echo "unqualified-search-registries = [\"docker.io\"]" | sudo tee -a /etc/containers/registries.conf
 
 sudo useradd -m -s /bin/bash podmanuser
-sudo su podmanuser
-cd
-#Setup containers Drives
-mkdir -p nginx nginx/data nginx/letsencrypt
 
-echo "version: '3.8'
-services:
-  app:
-    image: 'docker.io/jc21/nginx-proxy-manager:latest'
-    restart: unless-stopped
-    ports:
-      - '8080:80'
-      - '8081:81'
-      - '8443:443'
-    volumes:
-      - ./nginx/data:/data
-      - ./nginx/letsencrypt:/etc/letsencrypt" | tee -a docker-compose.yaml
-exit
-
-sudo touch /lib/systemd/system/podman-run.service
-echo "
-[Unit]
-Description=Podman-run
-Wants=network-online.target
-After=network-online.target
-
-[Service]
-User=podmanuser
-Group=podmanuser
-Type=oneshot
-RemainAfterExit=true
-ExecStartPre=/usr/bin/podman system prune -f
-ExecStart=/usr/bin/podman-compose -f /home/podmanuser/docker-compose.yaml up -d
-ExecStop=/user/bin/podman-compose down
-
-[Install]
-WantedBy=default.target" | sudo tee -a /lib/systemd/system/podman-run.service
-
-sudo systemctl --system daemon-reload
-
-sudo systemctl enable podman-run.service
-sudo systemctl start podman-run.service
-
+echo "For PiHole run"
+echo "sudo curl -sSL https://raw.githubusercontent.com/qoldaqgit/ubuntu_scripts/refs/heads/main/Apps/Podman/PiHole.sh | bash"
+echo ""
+echo "For Nginx Proxy Manager run"
+echo "sudo curl -sSL https://raw.githubusercontent.com/qoldaqgit/ubuntu_scripts/refs/heads/main/Apps/Podman/NginxProxy.sh | bash"
 #loginctl enable-linger podmanuser
 #delete stoped containers
 #podman container prune
