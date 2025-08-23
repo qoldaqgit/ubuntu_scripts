4f83b33cf6d08b7a112e37afd8c0d233a8103d6d
qoldaqgit <152833825+qoldaqgit@users.noreply.github.com>
2025-08-23 11:49:45 -0400

    Create PiHole.sh

---
+++ Apps/Podman/PiHole.sh
@@ -0,0 +1,77 @@
+#https://www.youtube.com/watch?v=69dADRzXpqk
+#sudo curl -sSL https://raw.githubusercontent.com/qoldaqgit/ubuntu_scripts/refs/heads/main/Apps/Podman/PiHole.sh | bash
+#!/bin/bash
+PODUSER="podmanuser"
+#Confirm user exist, if not create
+if ! id "$PODUSER" &>/dev/null; then
+    sudo useradd -m -s /bin/bash $PODUSER
+else
+    echo "User '$PODUSER' already exists."
+fi
+#Login into user home folder
+sudo su $PODUSER
+cd
+#Setup containers Drives
+mkdir -p pinhole pinhole/etc-pihole 
+MPASS="AdBlockDNS"
+#Create container compose file
+echo "# More info at https://github.com/pi-hole/docker-pi-hole/ and https://docs.pi-hole.net/
+services:
+  pihole:
+    container_name: pihole
+    image: pihole/pihole:latest
+    ports:
+      # DNS Ports
+      - "53:53/tcp"
+      - "53:53/udp"
+      # Default HTTP Port
+      - "80:80/tcp"
+      # Default HTTPs Port. FTL will generate a self-signed certificate
+      - "443:443/tcp"
+      # Uncomment the below if using Pi-hole as your DHCP Server
+      #- "67:67/udp"
+      # Uncomment the line below if you are using Pi-hole as your NTP server
+      #- "123:123/udp"
+    environment:
+      # Set the appropriate timezone for your location from
+      # https://en.wikipedia.org/wiki/List_of_tz_database_time_zones, e.g:
+      TZ: 'America/New_York'
+      # Set a password to access the web interface. Not setting one will result in a random password being assigned
+      FTLCONF_webserver_api_password: '$MPASS'
+      # If using Docker's default `bridge` network setting the dns listening mode should be set to 'all'
+      FTLCONF_dns_listeningMode: 'all'
+    # Volumes store your data between container upgrades
+    volumes:
+      # For persisting Pi-hole's databases and common configuration file
+      - './etc-pihole:/etc/pihole'
+      # Uncomment the below if you have custom dnsmasq config files that you want to persist. Not needed for most starting fresh with Pi-hole v6. If you're upgrading from v5 you and have used this directory before, you should keep it enabled for the first v6 container start to allow for a complete migration. It can be removed afterwards. Needs environment variable FTLCONF_misc_etc_dnsmasq_d: 'true'
+      #- './etc-dnsmasq.d:/etc/dnsmasq.d'
+    cap_add:
+      # See https://github.com/pi-hole/docker-pi-hole#note-on-capabilities
+      # Required if you are using Pi-hole as your DHCP server, else not needed
+      - NET_ADMIN
+      # Required if you are using Pi-hole as your NTP client to be able to set the host's system time
+      - SYS_TIME
+      # Optional, if Pi-hole should get some more processing time
+      - SYS_NICE
+    restart: unless-stopped" > /home/$PODUSER/pinhole/docker-compose.yaml
+
+#Create or add to the containers manager file
+cd ~/
+FILE="containers-manager.sh"
+LINE='
+#PiHole
+/usr/bin/podman-compose -f /home/$PODUSER/pinhole/docker-compose.yaml up -d
+#Ports #53,#80,#443'
+
+# Create file if it doesn't exist and set executable permissions
+if [ ! -f "$FILE" ]; then
+    touch "$FILE"
+    chmod +x "$FILE"
+    echo "#!/bin/bash" > "$FILE"
+fi
+
+# Append line to the file
+echo "$LINE" >> "$FILE"
+
+exit
