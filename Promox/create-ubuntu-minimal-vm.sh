95153e2ac9afaad2c5494c672ad294e8d17cdb5d
qoldaqgit <152833825+qoldaqgit@users.noreply.github.com>
2025-08-31 19:41:11 -0400

    Update create-ubuntu-minimal-vm.sh
    update ubuntu to 24.04

--- Promox/create-ubuntu-minimal-vm.sh
+++ Promox/create-ubuntu-minimal-vm.sh
@@ -1,6 +1,6 @@
 if [[ $# -eq 2 ]]
 then
-wget https://cloud-images.ubuntu.com/releases/22.04/release/ubuntu-22.04-server-cloudimg-amd64.img
+wget https://cloud-images.ubuntu.com/releases/24.04/release/ubuntu-24.04-server-cloudimg-amd64.img
 qm create $1 --memory 2048 --name $2 --net0 virtio,bridge=vmbr0
 qm disk import $1 ubuntu-22.04-server-cloudimg-amd64.img local-lvm
 qm set $1 --scsihw virtio-scsi-pci --scsi0 local-lvm:vm-$1-disk-0
