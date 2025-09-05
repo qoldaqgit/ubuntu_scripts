10a85d97d738c5226d8ba14b1311a2d65d4fae31
qoldaqgit <152833825+qoldaqgit@users.noreply.github.com>
2025-09-05 16:34:07 -0400

    Update podman_install.sh

--- Apps/Podman/podman_install.sh
+++ Apps/Podman/podman_install.sh
@@ -5,14 +5,17 @@
 sudo apt-get update
 sudo apt-get -y install podman podman-compose nano
 
-#systemctl --user enable --now podman.socket
-
 #Enable docker.io repository
 echo "unqualified-search-registries = [\"docker.io\"]" | sudo tee -a /etc/containers/registries.conf
 echo "net.ipv4.ip_unprivileged_port_start=40" | sudo tee -a /etc/sysctl.d/98-containers.conf
 sudo sysctl --system
 
+systemctl --user enable --now podman.socket
+
 sudo useradd -m -s /bin/bash podmanuser
+
+## Creating autorun
+/etc/systemd/system/podman-run.service
 
 echo "For PiHole run"
 echo "sudo curl -sSL https://raw.githubusercontent.com/qoldaqgit/ubuntu_scripts/refs/heads/main/Apps/Podman/PiHole.sh | bash"
