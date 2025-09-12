4080af3b198a082971caa8fef08c59410b3166c5
qoldaqgit <152833825+qoldaqgit@users.noreply.github.com>
2025-09-12 15:27:21 -0400

    Update create-ubuntu-minimal-vm.sh

--- Promox/create-ubuntu-minimal-vm.sh
+++ Promox/create-ubuntu-minimal-vm.sh
@@ -2,14 +2,20 @@
 
 if [[ $# -eq 2 ]]
 then
-wget https://cloud-images.ubuntu.com/releases/24.04/release/ubuntu-24.04-server-cloudimg-amd64.img
+FILE="ubuntu-24.04-server-cloudimg-amd64.img"
+if [ ! -f "$FILE" ]; then
+    wget https://cloud-images.ubuntu.com/releases/24.04/release/ubuntu-24.04-server-cloudimg-amd64.img
+    echo "Image created: $FILE"
+else
+    echo "Image already exists: $FILE"
+fi
+
 qm create $1 --memory 2048 --name $2 --net0 virtio,bridge=vmbr0
 qm disk import $1 ubuntu-24.04-server-cloudimg-amd64.img local-lvm
 qm set $1 --scsihw virtio-scsi-pci --scsi0 local-lvm:vm-$1-disk-0
 qm set $1 --ide2 local-lvm:cloudinit
 qm set $1 --boot c --bootdisk scsi0
 qm set $1 --serial0 socket --vga serial0
-rm ubuntu*
 echo "Done"
 
 else
