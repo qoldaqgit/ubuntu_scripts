ee82f73f9d7ac7e86d806c816a66b206c22d56ba
qoldaqgit <152833825+qoldaqgit@users.noreply.github.com>
2026-02-16 17:40:36 -0500

    Update traefik.sh

--- Apps/Podman/containers/traefik.sh
+++ Apps/Podman/containers/traefik.sh
@@ -3,7 +3,7 @@
 
 cd ~/containers
 #Setup containers Drives
-mkdir -p traefik
+mkdir -p traefik traefik/config
 cd traefik
 #Create container compose file
 cat > compose.yaml << 'EOF'
@@ -69,3 +69,114 @@ FQDN=sample.com
 EOF
 #Create cf-tocken
 echo "your-tocken-here" > cf-tocken
+
+cd config
+cat > config.yaml << 'EOF'
+http:
+  middlewares:    
+    default-security-headers:
+      headers:
+        customBrowserXSSValue: 0                            # X-XSS-Protection=1; mode=block
+        contentTypeNosniff: true                          # X-Content-Type-Options=nosniff
+        forceSTSHeader: true                              # Add the Strict-Transport-Security header even when the connection is HTTP
+        frameDeny: false                                   # X-Frame-Options=deny
+        referrerPolicy: "strict-origin-when-cross-origin"
+        stsIncludeSubdomains: true                        # Add includeSubdomains to the Strict-Transport-Security header
+        stsPreload: true                                  # Add preload flag appended to the Strict-Transport-Security header
+        stsSeconds: 3153600                              # Set the max-age of the Strict-Transport-Security header (63072000 = 2 years)
+        contentSecurityPolicy: "default-src 'self'"     
+        customRequestHeaders:
+          X-Forwarded-Proto: https
+    https-redirectscheme:
+      redirectScheme:
+        scheme: https
+        permanent: true
+# Middleware for Adding BASIC Authentication to the sites
+    test-auth:
+      basicAuth:
+        users:
+          - "admin:$2y$05$miO/T3.mzggzXOtCRFkDuuAHEh2SyX0K7arEFuFa1ncRg6FpAH7Fq"
+
+# Routes to external (not on same host) services
+  routers:
+    omada:
+      entryPoints:
+        - "https"
+      rule: "Host(`omada.your.domain`)"
+      middlewares:
+        - https-redirectscheme
+      tls: {}
+      service: omada
+    dockge:
+      entryPoints:
+        - "https"
+      rule: "Host(`dockge.your.domain`)"
+      middlewares:
+        - https-redirectscheme
+      tls: {}
+      service: dockge
+
+#Match Route with service
+  services:
+    omada:
+      loadBalancer:
+        servers:
+          - url: "https://server-ip:8043"
+        passHostHeader: true
+    dockge:
+      loadBalancer:
+        servers:
+          - url: "http://server-ip:5001"
+        passHostHeader: true
+EOF
+cat > traefik.yaml << 'EOF'
+api:
+  dashboard: true
+  debug: true
+entryPoints:
+  http:
+    address: ":80"
+    http:
+    #  middlewares: # uncomment if using CrowdSec - see my video
+    #    - crowdsec-bouncer@file
+      redirections:
+        entrypoint:
+          to: https
+          scheme: https
+  https:
+    address: ":443"
+    # http:
+    #  middlewares: # uncomment if using CrowdSec - see my video
+    #    - crowdsec-bouncer@file
+  # tcp:
+   # address: ":10000"
+  # apis:
+   # address: ":33073"
+serversTransport:
+  insecureSkipVerify: true
+providers:
+  docker:
+    endpoint: "unix:///var/run/docker.sock"
+    exposedByDefault: false
+  file:
+    filename: /config.yaml # example provided gives A+ rating https://www.ssllabs.com/ssltest/
+certificatesResolvers:
+  cloudflare:
+    acme:
+      # caServer: https://acme-v02.api.letsencrypt.org/directory # production (default)
+      # caServer: https://acme-staging-v02.api.letsencrypt.org/directory # staging (testing)
+      email: admin@your.domain # Cloudflare email (or other provider)
+      storage: acme.json
+      dnsChallenge:
+        provider: cloudflare # change as required
+        # disablePropagationCheck: true # Some people using Cloudflare note this can solve DNS propagation issues.
+        resolvers:
+          - "1.1.1.1:53"
+          - "1.0.0.1:53"
+
+log:
+  level: "INFO"
+  filePath: "/var/log/traefik/traefik.log"
+accessLog:
+  filePath: "/var/log/traefik/access.log"
+EOF
