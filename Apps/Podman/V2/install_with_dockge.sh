4d8311d76f22e9d9d7ed6a54705b5d8aa98af55f
qoldaqgit <152833825+qoldaqgit@users.noreply.github.com>
2026-02-16 00:00:12 -0500

    Update install_with_dockge.sh

--- Apps/Podman/V2/install_with_dockge.sh
+++ Apps/Podman/V2/install_with_dockge.sh
@@ -1,64 +1,43 @@
 #!/bin/bash
+#wget https://raw.githubusercontent.com/qoldaqgit/ubuntu_scripts/refs/heads/main/Apps/Podman/V2/install_with_dockge.sh
 sudo apt update && sudo apt upgrade -y
 sudo apt-get -y install podman podman-compose nano qemu-guest-agent
 
-mkdir dockge && cd dockge
-echo "services:
-  dockge:
-    image: louislam/dockge:1
-    restart: unless-stopped
-    ports:
-      # Host Port : Container Port
-      - 5001:5001
-    volumes:
-      - /run/user/1000/podman/podman.sock:/var/run/docker.sock
-      - ./data:/app/data
-      - /home/podman/dockge/stacks:/root/dockge/stacks
-    environment:
-      # Tell Dockge where is your stacks directory
-      - DOCKGE_STACKS_DIR=/root/dockge/stacks
-      # (Optional) Allow console managment
-      - DOCKGE_ENABLE_CONSOLE=true
-" > compose.yml
-cd ~
+##### Create Required Dir/Files #####
+cd
+mkdir .podman containers
+touch /home/podman/.podman/find_containers.sh
+echo "#!/bin/bash" > /home/podman/.podman/containers-manager-restart.sh
 
-##### Create the controllers files #######
-FILE="containers-manager-up.sh"
-LINE="
-#Dockge
-/usr/bin/podman-compose -f /home/podman/dockge/compose.yml up -d
-#Ports #5001"
+chmod 771 /home/podman/.podman/find_containers.sh
+chmod 771 /home/podman/.podman/containers-manager-restart.sh
 
-# Create file if it doesn't exist and set executable permissions
-if [ ! -f "$FILE" ]; then
-    touch "$FILE"
-    chmod +x "$FILE"
-    echo "#!/bin/bash" > "$FILE"
-fi
+##### Find Containers to restart on boot #######
+##### Scans /home/podman/restart folders for "compose.yaml" ######
+cat > /home/podman/.podman/find_containers.sh << 'EOF'
+#!/bin/bash
 
-# Append line to the file
-echo "$LINE" >> "$FILE"
+OUTPUT_FILE="/home/podman/.podman/containers-manager-restart.sh"
+PREFIX="/usr/bin/podman-compose -f "
+SUFFIX=" up -d"
 
-FILE="containers-manager-down.sh"
-LINE="
-#Dockge
-/usr/bin/podman-compose -f /home/podman/dockge/compose.yml down
-#Ports #5001"
+echo "#!/bin/bash"> "$OUTPUT_FILE"
 
-# Create file if it doesn't exist and set executable permissions
-if [ ! -f "$FILE" ]; then
-    touch "$FILE"
-    chmod +x "$FILE"
-    echo "#!/bin/bash" > "$FILE"
-fi
+find "/home/podman/containers" -type f \( -name "compose.yaml" -o -name "composer.yml" \) | \
+while read -r file; do
+    if grep -qE '^ {4}restart: (unless-stopped|always)$' "$file"; then
+        echo "${PREFIX}${file}${SUFFIX}" >> "$OUTPUT_FILE"
+    fi
+done
 
-# Append line to the file
-echo "$LINE" >> "$FILE"
+podman image rm -a
+podman stop -a
+podman system prune -f
+EOF
 
-
-##### Create Service for auto restart #########
+##### Create Service for AutoRun #########
 sudo bash -c 'echo "[Unit]
-Description=Podman-run
+Description=Podman-autorun
 Wants=network-online.target
 After=network-online.target
 
@@ -69,14 +48,42 @@ Type=oneshot
 RemainAfterExit=true
 
 ExecStartPre=/usr/bin/podman system prune -f
-ExecStart=/home/podman/containers-manager-up.sh     
-ExecStop=/home/podman/containers-manager-down.sh     
+ExecStart=/home/podman/.podman/containers-manager-restart.sh   
+ExecStop=/home/podman/.podman/find_containers.sh
 
 [Install]
 WantedBy=default.target
-" > /etc/systemd/system/podman-run.service'
+" > /etc/systemd/system/podman-autorun.service'
 
 sudo systemctl --system daemon-reload
-sudo systemctl enable podman-run.service
+sudo systemctl enable podman-autorun.service
+sudo systemctl start podman-autorun.service
 
+cd
+##### Install Dockge #####
+mkdir containers/dockge && cd containers/dockge
+echo "services:
+  dockge:
+    image: louislam/dockge:1
+    container_name: dockge
+    restart: unless-stopped
+    ports:
+      # Host Port : Container Port
+      - 5001:5001
+    volumes:
+      - /run/user/1000/podman/podman.sock:/var/run/docker.sock
+      - ./data:/app/data
+      - ../:/root/dockge/stacks
+    environment:
+      # Tell Dockge where is your stacks directory
+      - DOCKGE_STACKS_DIR=/root/dockge/stacks
+      # (Optional) Allow console managment
+      - DOCKGE_ENABLE_CONSOLE=true
+" > compose.yaml
 
+sudo systemctl stop podman-autorun.service
+sudo systemctl start podman-autorun.service
+
+cd ~
+clear
+echo "Installation Completed"
