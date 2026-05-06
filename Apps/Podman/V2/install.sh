bb63f0573e1bd4793440457a0aff02dd422d5dd7
qoldaqgit <152833825+qoldaqgit@users.noreply.github.com>
2026-05-06 15:58:05 -0400

    Update install.sh

--- Apps/Podman/V2/install.sh
+++ Apps/Podman/V2/install.sh
@@ -1,7 +1,35 @@
 #!/bin/bash
 #wget https://raw.githubusercontent.com/qoldaqgit/ubuntu_scripts/refs/heads/main/Apps/Podman/V2/install.sh
+
+echo "Let's install Podman Rootless! but before we need to answer the following questions."
+
+# Ask questions
+read -p "Need PORTS under 1024 open (Restricted by default)? (y/n) " RESTRICTED_PORTS
+read -p "Need composer/npm? (y/n) " EXTRA_TOOLS
+read -p "Want to install Dockge (Podman Web UI)? (y/n) " DOCKGE
+
+# Confirm input
+echo ""
+echo "Summary:"
+echo "Need PORTS under 1024: $RESTRICTED_PORTS"
+echo "Need composer/npm: $EXTRA_TOOLS"
+echo "install Dockge: $DOCKGE"
+echo ""
+
+read -p "Proceed with these settings? (y/n) " CONFIRM
+
+if [[ "$CONFIRM" != "y" ]]; then
+    echo "Aborted."
+    exit 1
+fi
+
+
+
 sudo apt update && sudo apt upgrade -y
-sudo apt-get -y install podman podman-compose composer npm nano qemu-guest-agent
+sudo apt-get -y install podman podman-compose nano qemu-guest-agent
+if [[ "$EXTRA_TOOLS" == "y" ]]; then
+sudo apt-get -y install composer npm
+fi
 echo "unqualified-search-registries = [\"docker.io\"]" | sudo tee -a /etc/containers/registries.conf
 
 ##### Create Required Dir/Files #####
@@ -9,65 +37,105 @@ sudo mkdir /app /app/stacks /app/.podman /app/data
 sudo chmod 755 /app
 sudo chown -R $USER:$USER /app/stacks /app/.podman /app/data
 sudo chmod 700 /app/stacks /app/.podman /app/data
-touch /app/.podman/find_containers.sh
-echo "#!/bin/bash" > /app/.podman/containers-manager-restart.sh
 
-##### Find running Containers to restart on boot (current running)#######
-cat > /app/.podman/find_containers.sh << 'EOF'
+echo "#!/bin/bash" > /app/.podman/container-start.sh
+echo "#!/bin/bash" > /app/.podman/container-start.sh
+cat << EOF  > ~/containers2restart.sh
 #!/bin/bash
-OUTPUT_FILE="/app/.podman/containers-manager-restart.sh"
-echo "#!/bin/bash
-" > "$OUTPUT_FILE"
-podman ps -a --format "{{.Names}}" | {
-  first=true
-  while IFS= read -r line; do
-    if [ "$first" = true ]; then
-      printf "podman start %s" "$line"
-      first=false
-    else
-      printf " && podman start %s" "$line"
+if [ "$#" -eq 2 ]; then
+
+    if [ ! -f "/app/.podman/container-start.sh" ]; then
+        echo "#!/bin/bash
+        " > /app/.podman/container-start.sh
+        sudo chmod 771 /app/.podman/container-start.sh
+        echo "/app/.podman/container-start.sh created"
     fi
-  done
-  echo ""
-} >> "$OUTPUT_FILE"
-/app/.podman/containers-manager-restart.sh
+    if [ ! -f "/app/.podman/container-stop.sh" ]; then
+        echo "#!/bin/bash
+        " >  /app/.podman/container-stop.sh
+        sudo chmod 771 /app/.podman/container-stop.sh
+        echo "/app/.podman/container-stop.sh created"
+    fi
+
+    if [ "$1" = "-start" ]; then
+        if [[ -f $2 && ( $2 == *.yml || $2 == *.yaml ) ]]; then
+
+            if ! grep -Fxq "/usr/bin/podman-compose -f $2 up -d" "/app/.podman/container-start.sh"; then
+                echo "/usr/bin/podman-compose -f $2 up -d" >> "/app/.podman/container-start.sh"
+                echo "Added to container-start"
+            else
+                echo "Skipping, already exist in container-start"
+            fi
+
+            if ! grep -Fxq "/usr/bin/podman-compose -f $2 down" "/app/.podman/container-stop.sh"; then
+                echo "/usr/bin/podman-compose -f $2 down" >> "/app/.podman/container-stop.sh"
+                echo "Added to container-stop"
+            else
+                echo "Skipping, already exist in container-stop"
+            fi
+
+        else
+            echo "Invalid or missing file"
+        fi
+    fi
+
+    if [ "$1" = "-stop" ]; then
+    
+        grep -Fv "/usr/bin/podman-compose -f $2 up -d" /app/.podman/container-start.sh > tmp && mv tmp /app/.podman/container-start.sh
+        grep -Fv "/usr/bin/podman-compose -f $2 down" /app/.podman/container-stop.sh > tmp && mv tmp /app/.podman/container-stop.sh
+        echo "Removed"
+    fi
+
+else
+    echo "[-start/-stop] [path/to/compose/file]"
+fi
 EOF
 
 #### Set proper permission to files #####
-chmod 771 /app/.podman/find_containers.sh
-chmod 771 /app/.podman/containers-manager-restart.sh
+sudo chmod 771 /app/.podman/container-start.sh
+sudo chmod 771 /app/.podman/container-start.sh
+sudo chmod 771 ~/containers2restart.sh
 
 ##### Allow Lower ports for rootless Containers from (>1024) to (>=80) #####
+if [[ "$RESTRICTED_PORTS" == "y" ]]; then
 sudo sysctl -w net.ipv4.ip_unprivileged_port_start=80
 echo "net.ipv4.ip_unprivileged_port_start=80" | sudo tee -a /etc/sysctl.conf
-
+fi
 ##### Setup user enviroment #####
 sudo loginctl enable-linger $USER
 systemctl --user enable --now podman.socket
 
 ##### Create Service for AutoRun #########
 bash -c 'echo "[Unit]
-Description=Podman-autorun
+Description=Podman container-traefik.service
+Documentation=man:podman-generate-systemd(1)
+Wants=network-online.target
 After=podman.service
-Before=shutdown.target reboot.target halt.target podman.service
 
 [Service]
-User=->Here<-
-Group=->Here<-
+Environment=PODMAN_SYSTEMD_UNIT=%n
+Restart=on-failure
 Type=oneshot
 RemainAfterExit=true
 
-ExecStartPre=
-ExecStart=/app/.podman/find_containers.sh  
-ExecStop=podman stop -a
+TimeoutStopSec=70
+ExecStart=/app/.podman/container-start.sh 
+ExecStop=/app/.podman/container-stop.sh 
 
 [Install]
 WantedBy=default.target
 " > podman-autorun.service'
-sed -i "s/->Here<-/$USER/g" podman-autorun.service
-sudo mv podman-autorun.service /etc/systemd/system/podman-autorun.service
+
+mv podman-autorun.service /home/vision/.config/systemd/user/podman-autorun.service
+systemctl --user daemon-reload
+systemctl --user enable podman-autorun.service
+systemctl --user start podman-autorun.service
+
+
 
 ##### Install Dockge #####
+
+if [[ "$DOCKGE" == "y" ]]; then
 cd /app/data
 echo "services:
   dockge:
@@ -95,15 +163,6 @@ networks:
 podman network create   --subnet 10.69.10.0/24   --gateway 10.69.10.1   intra_net
 podman-compose up -d
 
-##### Setup the auto restart #####
-#sudo systemctl stop podman-autorun.service
-#sudo systemctl disable podman-autorun.service
-
-sudo systemctl --system daemon-reload
-sudo systemctl enable podman-autorun.service
-sudo systemctl start podman-autorun.service
-#sudo systemctl status podman-autorun.service
-
 cd ~
 
 ##### Provide info to user #####
@@ -116,5 +175,10 @@ if curl -s --head http://$IPHOST:5001 | head -n 1 | grep "200" > /dev/null; then
   echo -e "\e[0;32m[~] Please visit http://$IPHOST:5001 to complete the initall setup wizard.\e[0m\n"
 else
   echo "Install Failed"
+fi
+else
+  clear
+  podman ps
+  echo -e "\e[0;32m[~] Podman Rootless has been successfully installed!\e[0m"
 fi
 
