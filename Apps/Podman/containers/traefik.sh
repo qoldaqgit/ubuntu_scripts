a4a313e62d8e9396ecf37e270d636494e9dc8200
qoldaqgit <152833825+qoldaqgit@users.noreply.github.com>
2026-05-07 18:24:35 -0400

    Update traefik.sh

--- Apps/Podman/containers/traefik.sh
+++ Apps/Podman/containers/traefik.sh
@@ -1,7 +1,7 @@
-#sudo curl -sSL https://raw.githubusercontent.com/qoldaqgit/ubuntu_scripts/refs/heads/main/Apps/Podman/containers/traefik.sh | bash
+#curl -sSL https://raw.githubusercontent.com/qoldaqgit/ubuntu_scripts/refs/heads/main/Apps/Podman/containers/traefik.sh | bash
 #!/bin/bash
 
-cd /app/stacks
+cd containers
 #Setup containers Drives
 mkdir -p traefik traefik/config
 cd traefik
