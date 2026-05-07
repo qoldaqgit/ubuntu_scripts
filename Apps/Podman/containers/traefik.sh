7d41bdcf081ce0b64800626ee73a2b5271254003
qoldaqgit <152833825+qoldaqgit@users.noreply.github.com>
2026-05-07 18:41:12 -0400

    Update traefik.sh

--- Apps/Podman/containers/traefik.sh
+++ Apps/Podman/containers/traefik.sh
@@ -7,9 +7,6 @@ mkdir -p traefik traefik/config
 cd traefik
 #Create container compose file
 cat > compose.yaml << 'EOF'
-secrets:
-  cf-token:
-    file: ./cf-token
 services:
   traefik:
     image: traefik:latest # or traefik:v3.3 to pin a version
@@ -17,8 +14,6 @@ services:
     restart: unless-stopped
     security_opt:
       - no-new-privileges:true # helps to increase security
-    secrets:
-      - cf-token # the secret at the top of this file
     env_file:
       - .env # store other secrets e.g., dashboard password
     networks:
@@ -26,31 +21,28 @@ services:
     ports:
       - 80:80
       - 443:443
-    # - 10000:10000 # optional
-    # - 33073:33073 # optional
     environment:
       - TRAEFIK_DASHBOARD_CREDENTIALS=${TRAEFIK_DASHBOARD_CREDENTIALS}
-      # - CF_API_EMAIL=your@email.com # Cloudflare email
-      # - CF_DNS_API_TOKEN=YOUR-TOKEN # Cloudflare API Token
-      - CF_DNS_API_TOKEN_FILE=/run/secrets/cf-token # see https://doc.traefik.io/traefik/https/acme/#providers
+      - CF_API_EMAIL=${CF_API_EMAIL} # Cloudflare email
+      - CF_DNS_API_TOKEN=${CF_DNS_API_TOKEN} # Cloudflare API Token
       # token file is the proper way to do it
     volumes:
       - /etc/localtime:/etc/localtime:ro
       - /run/user/1000/podman/podman.sock:/var/run/docker.sock:ro
-      - /root/dockge/stacks/traefik/config/traefik.yaml:/traefik.yaml:ro
-      - /root/dockge/stacks/traefik/config/acme.json:/acme.json
-      - /root/dockge/stacks/traefik/config/config.yaml:/config.yaml:ro
-      - /root/dockge/stacks/traefik/config/logs:/var/log/traefik
+      - ./config/traefik.yaml:/traefik.yaml:ro
+      - ./config/acme.json:/acme.json
+      - ./config/config.yaml:/config.yaml:ro
+      - ./config/logs:/var/log/traefik
     labels:
       - traefik.enable=true
       - traefik.http.routers.traefik.entrypoints=http
-      - traefik.http.routers.traefik.rule=Host(`traefik.${FQDN}`)
+      - traefik.http.routers.traefik.rule=Host(`${SUBDOMAIN}.${FQDN}`)
       - traefik.http.middlewares.traefik-auth.basicauth.users=${TRAEFIK_DASHBOARD_CREDENTIALS}
       - traefik.http.middlewares.traefik-https-redirect.redirectscheme.scheme=https
       - traefik.http.middlewares.sslheader.headers.customrequestheaders.X-Forwarded-Proto=https
       - traefik.http.routers.traefik.middlewares=traefik-https-redirect
       - traefik.http.routers.traefik-secure.entrypoints=https
-      - traefik.http.routers.traefik-secure.rule=Host(`traefik.${FQDN}`)
+      - traefik.http.routers.traefik-secure.rule=Host(`${SUBDOMAIN}.${FQDN}`)
       - traefik.http.routers.traefik-secure.middlewares=traefik-auth
       - traefik.http.routers.traefik-secure.tls=true
       - traefik.http.routers.traefik-secure.tls.certresolver=cloudflare
@@ -58,8 +50,9 @@ services:
       - traefik.http.routers.traefik-secure.tls.domains[0].sans=*.${FQDN}
       - traefik.http.routers.traefik-secure.service=api@internal
 networks:
-  proxy:
-    #external: true # or uncomment this line to stop auto create the network
+  intra_net:
+    external: true # or uncomment this line to stop auto create the network
+
 EOF
 
 #Create .env file
